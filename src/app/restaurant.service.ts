67a54182bb4d2ba7b0d23aef2e50c0669e0583f8
VASCONCELOS NEVES João Pedro <jpvasconcelosneves@europe.corp.altran.com>
2019-12-04 11:00:34 +0000

    Added prod backend link

--- src/app/restaurant.service.ts
+++ src/app/restaurant.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
-  url = 'http://localhost:1337/restaurants';
+  devUrl = 'http://localhost:1337/restaurants';
+  prodUrl = 'https://restaurant-cms-strapi.herokuapp.com/restaurants';
 
   constructor(private http: HttpClient) { }
 
+  getUrl(){
+    return isDevMode() ? this.devUrl : this.prodUrl;
+  }
+
   getRestaurants() {
-    return this.http.get(this.url);
+    return this.http.get(this.getUrl());
   }
 }
